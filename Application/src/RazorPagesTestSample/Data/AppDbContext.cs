b40a61de007d9ff8000067a2be08012322115b38
Shivinder <6997655+shivinder@users.noreply.github.com>
2023-02-11 14:36:38 +1000

    testing push workflow trigger

--- Application/src/RazorPagesTestSample/Data/AppDbContext.cs
+++ Application/src/RazorPagesTestSample/Data/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace RazorPagesTestSample.Data
 {
     public class AppDbContext : DbContext
     {
+
         public AppDbContext(DbContextOptions<AppDbContext> options) 
             : base(options)
         {
